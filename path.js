5ee706be579cea5afb6f21c3b9cfc1de00bba313
Alejandro-Quiceno-Perez <alejo.perez2905@gmail.com>
2025-12-10 11:51:33 -0500

    Se actualiza el curso donde se vio mosulo fs y async

--- path.js
+++ path.js
@@ -1,3 +1,11 @@
 const path = require('path');
 
-console.log(path)
+const filePath = path.join('/public', 'dist', '/styles', 'main.css');
+
+console.log(filePath); // Imprime la ruta completa del archivo
+console.log(path.basename(filePath)); // Imprime el nombre del archivo: 'main.css'
+console.log(path.dirname(filePath)); // Imprime el directorio del archivo: '/public/dist/styles'
+console.log(path.extname(filePath)); // Imprime la extensión del archivo: '.css'
+console.log(path.parse(filePath)); // Imprime un objeto con detalles del archivo
+
+console.log(path.resolve('dist')); // Imprime la ruta absoluta al directorio 'dist' desde el directorio actual
